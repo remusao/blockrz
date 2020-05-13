436a06af69b9b23446220cf03ec67be384c242fb
Rémi Berson <remi@cliqz.com>
2020-05-13 13:15:48 +0200

    Add minification

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,11 +2,17 @@ import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import sourcemaps from 'rollup-plugin-sourcemaps';
+import { terser } from 'rollup-plugin-terser';
 
 const plugins = [
   typescript(),
   resolve(),
   commonjs(),
+  terser({
+    output: {
+      comments: false,
+    },
+  }),
   sourcemaps(),
 ];
 
